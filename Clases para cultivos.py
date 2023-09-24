3111e8d4907b4b41a10275db71d1538b2d29588a
David Ixquiac <davidixquiac212@gmail.com>
2023-09-23 18:06:52 -0600

    Cultivos
    Clase cultivos

--- Clases para cultivos.py
+++ Clases para cultivos.py
@@ -46,11 +46,57 @@ def datos_para_trigo():
     trigo1.plantar()
     trigo1.crecimiento()
     trigo1.cosechar()
+    
 
-datos_para_trigo()
+def datos_para_tomate():
+    print('Recuerda que si estas iniciando comienzas con 50')
+    siembra = int(input('Ingresa la cantidad de semillas que utilizaras: '))
+    cosecha = siembra
+    tomate1 = trigo(siembra,60,cosecha,50,180)
+    tomate1.plantar()
+    tomate1.crecimiento()
+    tomate1.cosechar()
 
+def datos_para_cebolla():
+    print ('Recuerda que si estas iniciando comienzas con 50')
+    siembra = int(input('Ingresa la cantidad de semillas que utilizaras: '))
+    cosecha = siembra
+    cebolla1 = trigo(siembra,60,cosecha,50,240)
+    cebolla1.plantar()
+    cebolla1.crecimiento()
+    cebolla1.cosechar()
 
+def datos_para_zanahoria():
+    print ('Recuerda que si estas iniciando comienzas con 50')
+    siembra = int(input('Ingresa la cantidad de semillas que utilizaras: '))
+    cosecha = siembra
+    zanahoria = trigo(siembra,60,cosecha,50,60)
+    zanahoria.plantar()
+    zanahoria.crecimiento()
+    zanahoria.cosechar()
 
+def datos_para_maiz():
+    print ('Recuerda que si estas iniciando comienzas con 50')
+    siembra = int(input('Ingresa la cantidad de semillas que utilizaras: '))
+    cosecha = siembra
+    maiz= trigo(siembra,60,cosecha,50,160 )
+    
 
+print('Menu para plantas')
+print('1. Plantar trigo')
+print('2. Plantar tomate')
+print('3. Plantar cebollas')
+print('4. Plantar Zanahoria')
+opcion = int(input('Ingrese la opcion que desea: '))
+match opcion:
+    case 1:
+        datos_para_trigo()
+    case 2:
+        datos_para_tomate()
+    case 3:
+        datos_para_cebolla()
+    case 4:
+        datos_para_zanahoria()
+    
     
 
